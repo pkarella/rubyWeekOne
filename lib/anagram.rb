a646734ff3089b99fd3b0a2ed8b0a859a875f55c
peter <pkarella27@yahoo.com>
2017-04-21 16:52:00 -0700

    rfactor

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,14 +5,20 @@ word= self.downcase.delete(' ').split("")
 wordtwo= wordtwo.downcase.delete(' ').split("")
 wordarrayone=[]
 both_words = word & wordtwo
-# if (both_words&&vowels).empty?()
-#   wordarrayone.push("Please enter a valid word")
-# end
+
+both_words.each() do |vowel|
+if both_words.length == vowel.length
+ wordarrayone.push("this is not a word")
+end
+
+end
+
+
 if both_words.length == 0
   wordarrayone.push("These words have no letter matches this is an antigram")
-elsif word.uniq.sort == wordtwo.uniq.sort
+elsif word.sort == wordtwo.uniq.sort
 wordarrayone.push("its an anagram")
-else word.uniq.sort != wordtwo.uniq.sort
+else word.sort != wordtwo.uniq.sort
   wordarrayone.push("not an anagram")
 end
 if word == wordtwo.reverse
